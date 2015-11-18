5a022b621c9e1b16039b5d782555a318c2e6b956
jimmi <jimmirbn@gmail.com>
2015-11-18 22:27:38 +0100

    add new test page

--- src/templating/page.php
+++ src/templating/page.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 <div id="main-content" role="main" class="container">
 		<h1>Test</h1>
+
 <button onclick="ga('send', 'event', 'Category CTA knap', 'click på knap', 'En valgfri value');">Track click</button>
+<button onclick="ga('send', 'event', 'Ny cat cta knap', 'Click på knap 2', 'Ny test');">Track click 2</button>
 </div><!-- /main-content -->    
 <?php get_footer(); ?>
